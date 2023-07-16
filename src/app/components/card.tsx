02c17a4f457ed0def6fa212d4b813f7160cb0355
Lukas Rasocha <rasocha.lukas@gmail.com>
2023-07-16 13:34:58 +0200

    feat(card.tsx): add Card component
    A new file `card.tsx` was added to the `src/app/components` directory. This file contains a functional component called `Card` that renders a card with a title, image, link, and tags. The `Card` component is used to display project cards in the application.

---
+++ src/app/components/card.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import Link from 'next/link';
+
+interface CardProps {
+  title: string;
+  image: string;
+  link: string;
+  tags: string[];
+}
+
+export default function Card(
+  { 
+  title,
+  image,
+  link,
+  tags
+  }: CardProps
+  ) {
+
+  return (
+    <div className="card w-96 bg-base-300 shadow-xl">
+      <figure className="h-64 w-full overflow-hidden relative">
+        <img src={`/projects/${image}`} alt={title} className="h-full w-full object-cover absolute"/>
+      </figure>
+      <div className="card-body">
+        <h2 className="font-mono card-title">{title}
+        </h2>
+        <div className='font-bold flex flex-wrap'>
+          {tags.map((tag, index) => (
+            <div key={index} className="badge badge-primary mr-2 mb-2">{tag}</div>
+          ))}
+        </div>
+        <div className="card-actions justify-center">
+          <Link href={link}>
+            <button className="font-mono btn bg-base-200 btn-primary mt-5">See more</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
