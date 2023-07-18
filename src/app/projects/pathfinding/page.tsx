93f589c32b32caf53333a03b691ff30f006f5295
Lukas Rasocha <rasocha.lukas@gmail.com>
2023-07-18 21:17:37 +0200

    feat(pathfinding): add pathfinding visualizer page
    This commit adds a new file `page.tsx` to the `src/app/projects/pathfinding` directory. The page component renders a pathfinding visualizer with a title, breadcrumbs, date, badges, description, and an image. The purpose of this page is to showcase the implementation of four different graph-searching algorithms: A star, BFS, DFS, and Dijkstra. The algorithms are tested on randomly generated maps of different sizes to compare their performance and verify their theoretical big-O. The implementation of the algorithms can be found [here](http://webkutil.cz/algorithms/) and the research paper can be read [here](https://rucforsk.ruc.dk/ws/files/68132039/BP2_Group_1_Pathfinding__new___4_.pdf).
    
    feat(zelda): update description of zelda project
    
    This commit updates the description of the Zelda project in the `page.tsx` file

---
+++ src/app/projects/pathfinding/page.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image"
+
+export default function Page(){
+  return (
+  <div className="w-full md:w-1/2">
+    <div className="text-sm breadcrumbs text-gray-400">
+      <ul>
+        <li><a href="/">Home</a></li> 
+        <li>Projects</li> 
+        <li>Pathfinding visualizer 🗺️</li>
+      </ul>
+    </div>
+      <h1 className="text-4xl font-bold pt-2">Pathfinding visualizer 🗺️️</h1>
+      <p className="pt-5 text-gray-400">Jun 30, 2020</p>
+
+      <div className='font-bold flex flex-wrap pt-5'>
+        <div className="badge badge-primary mr-2 mb-2">Algorithms & Data Structures</div>
+        <div className="badge badge-primary mr-2 mb-2">2020</div>
+      </div>
+
+      <p className="font-mono pt-10 text-gray-400">
+        In this project we used JavaScript to implement four different graph-searching algorithms: <span className="text-white">A star, BFS, DFS, Dijkstra</span>. We put these algorithms to the test on a range of randomly generated maps of different sizes, gathering data to see how they stack up against each other and to verify their theoretical <span className="text-white">big-O</span>. If you're interested, you can check out the implementations <a className="text-purple-500 hover:underline" href="http://webkutil.cz/algorithms/" target="_blank" rel="noopener noreferrer">here</a>.
+        Or read the paper <a className="text-purple-500 hover:underline" href="https://rucforsk.ruc.dk/ws/files/68132039/BP2_Group_1_Pathfinding__new___4_.pdf" target="_blank" rel="noopener noreferrer">here</a>.
+      </p>
+
+      <Image
+        className="pt-5"
+        src="/projects/pathfinding/system.png" 
+        alt="pathfinding-visualizer"
+        width={600}
+        height={400}
+      />
+    </div>
+  )
+}
